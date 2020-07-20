90c5ea256f24b0e429f206eaf275c642062650d1
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-07-20 16:24:03 +0200

    root error solved

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Query } from "@nestjs/common";
 
 @Controller()
 export class AppController {
+  constructor () {}
+
+  @Get()
   getHello(): string {
     return "Hello World!";
   }
-  constructor() {}
 }
