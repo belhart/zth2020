6b4da9f2d0e7cfdc93e636174ec8e4352872fb83
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-07-21 13:32:36 +0200

    database connection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as PostgressConnectionStringParser from "pg-connection-string";
 import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  Logger.log(process.env.DATABASE_URL);
+  const databaseUrl: string = process.env.DATABASE_URL;
+  const connectionOptions = PostgressConnectionStringParser.parse(databaseUrl);
+  Logger.log(connectionOptions);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
