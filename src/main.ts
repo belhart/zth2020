2356f4270e751527c02549546144cb5c2c34cf14
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-07-21 14:02:54 +0200

    database connection

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,29 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as PostgressConnectionStringParser from "pg-connection-string";
 import { Logger } from "@nestjs/common";
+import { ConnectionOptions, createConnection } from "typeorm";;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const databaseUrl: string = process.env.DATABASE_URL;
   const connectionOptions = PostgressConnectionStringParser.parse(databaseUrl);
   Logger.log(connectionOptions);
+  const typeOrmOptions: ConnectionOptions= {
+    type: "postgres",
+    host: connectionOptions.host,
+    port: parseInt(connectionOptions.port),
+    username: connectionOptions.user,
+    password: connectionOptions.password,
+    database: connectionOptions.database,
+    logging: true,
+    synchronize: true,
+    entities: ["./src/**/*.entity.ts", "./dist/**/*.entity.js"],
+    extra: {
+        ssl: true
+    }  
+  };
+  const connection = createConnection(typeOrmOptions);
+  Logger.log(connection);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
