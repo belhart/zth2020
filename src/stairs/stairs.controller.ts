2af9c1b89c2a32212999298aff3e6be58e22dc9a
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-08-02 14:35:55 +0200

    stairs are now accounting for missing steps

--- src/stairs/stairs.controller.ts
+++ src/stairs/stairs.controller.ts
@@ -6,28 +6,60 @@ export class StairsController {
     getNumberOfSteps(@Query() query): Number{
         if ((Object.keys(query).indexOf("numberOfStair") > -1) && (Object.keys(query).indexOf("stepSizeList") > -1)){
             var steps = query.stepSizeList.split(",");
-            return countWays(query.numberOfStair,steps[steps.length-1]);
+            steps.sort(function(a, b) {
+                return Number(a) - Number(b);
+            });
+            if (steps.length !== Number(steps[steps.length - 1])){
+                 const missingSteps = GetMissingSteps(steps); // check if a step is missing
+                 return CountWaysMissingSteps(missingSteps, query.numberOfStair, steps[steps.length - 1]);
+            }
+            return CountWays(query.numberOfStair,steps[steps.length-1]);
         }
         throw new BadRequestException("Not all required query parameters provided");
     }
 }
 
-function countWaysUtil2(n: Number, m: Number){
+function CountWaysUtilNoMissingSteps(n: Number, m: Number){
     let res = new Object();
-    Logger.log(n);
     res[0] = 1; 
     res[1] = 1; 
     for (let i = 2; i < n; i++) { 
         res[i] = 0; 
         for (let j = 1; j <= m && j <= i; j++) 
             res[i] += res[i - j]; 
-    } 
-    Logger.log(res);
+    }
     return res[Number(n) - 1]; 
 }
 
+function CountWaysUtilMissingSteps(n: Number, m: Number, missingSteps){
+    let res = new Object();
+    res[0] = 1; 
+    res[1] = 1; 
+    for (let i = 2; i < n; i++) { 
+        res[i] = 0; 
+        for (let j = 1; j <= m && j <= i; j++){
+            if (missingSteps.indexOf(j) < 0) res[i] += res[i - j];
+        }
+    }
+    return res[Number(n) - 1]; 
+}
 
-function countWays(s: Number, m: Number) 
+function CountWays(s: Number, m: Number) 
 { 
-    return countWaysUtil2(Number(s) + 1, m); 
+    return CountWaysUtilNoMissingSteps(Number(s) + 1, m); 
+}
+
+function CountWaysMissingSteps(missingSteps, s:Number, m:Number)
+{ 
+    return CountWaysUtilMissingSteps(Number(s) + 1, m, missingSteps);
+}
+
+function GetMissingSteps(steps){
+    const missingSteps = new Array<Number>();
+    for(var i = 2; i < steps[steps.length - 1]; i++)
+    { 
+        if(Object.keys(steps).indexOf(i.toString()) < 0) missingSteps.push(i);
+    }
+
+    return missingSteps;
 }
